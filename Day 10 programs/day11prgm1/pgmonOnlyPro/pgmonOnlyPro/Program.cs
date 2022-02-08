a109c11c99aadf5f969a6fb26a6079a2bceaa5c6
pallavimechineni <pallavi.velma@gmail.com>
2022-02-08 12:42:35 +0530

    Day11 Assignment

---
+++ Day 10 programs/day11prgm1/pgmonOnlyPro/pgmonOnlyPro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace pgmonOnlyPro
+{
+    /***************************************
+    Author: M.Pallavi
+    purpose: A sample code using only properties */
+
+
+    internal class Employee1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Designation
+            { get; set; }
+        public int Salary
+        {
+            get
+            {
+                return (Designation == "S") ? 3000 : 6000;
+               
+            }
+
+
+        }
+
+    
+    static void Main(string[] args)
+        {
+            
+        Employee1 emp = new Employee1();
+        emp.Id = 15;
+        Console.WriteLine(emp.Id);
+        emp.Designation = "L";
+        Console.WriteLine(emp.Salary);
+
+    }
+}
+}
---
+++ Day 10 programs/day11prgm1/pgmonStaticmethods/pgmonStaticmethods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace pgmonStaticmethods
+{   /********************************************************************************
+     Author : M.Pallavi
+    purpose : Create Mathematics class and add 3 static methods and call the methods in main method.*/
+    class Mathematics
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+
+        }
+        public static int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("addition is "+Mathematics.Add(5, 6));
+                Console.WriteLine("substraction is "+Mathematics.Sub(9, 1));
+                Console.WriteLine("multiplication is "+Mult(5, 6));
+
+
+
+            }
+        }
+    }
+}
