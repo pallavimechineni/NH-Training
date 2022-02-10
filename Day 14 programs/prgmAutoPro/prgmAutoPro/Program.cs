90c8d194d11e77906700561339a15d18ebcdd95f
pallavimechineni <pallavi.velma@gmail.com>
2022-02-10 18:59:45 +0530

    program to illustrate on Auto implemented properties

---
+++ Day 14 programs/prgmAutoPro/prgmAutoPro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pgm
+{    class autop
+    {
+        public string name { get; set; }
+        public string location { get; set; }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            autop data = new autop();
+            data.name = "Ram";
+            data.location = "hyd";
+            Console.WriteLine("name is " + data.name);
+            Console.WriteLine("lacation is " +data. location);
+
+                
+        }
+    }
+}
