306c776e05d3a9f370904e8db75e1704634ecfa3
pallavimechineni <pallavi.velma@gmail.com>
2022-02-10 18:57:28 +0530

    program to illustrate simple properties

---
+++ Day 14 programs/ConsoleApp1/ProgramonProperties.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        private int a;
+        private int b;
+        public int A
+        {
+            set { a = value; }
+            get { return a; }
+        }
+
+        public int B
+        {
+            set { b = value; }
+            get { return b; }
+        }
+
+
+        public static void Main(string[] args)
+        {
+
+            Program p = new Program();
+            p.A = 23;
+            p.B = 54;
+            Console.WriteLine("value of a is" + p.A);
+            Console.WriteLine("value of b is"+ p.B);
+        }
+
+    }
+}
+    
