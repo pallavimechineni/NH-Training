7aad91310dabe61a7e82ac8d153597cad8bb1087
pallavimechineni <pallavi.velma@gmail.com>
2022-02-11 22:52:57 +0530

    program to check prime number using Break statement

---
+++ Day 14 programs/pgmonPrime_num/pgmonPrime_num/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pgmonPrime_num
+{  /*****************************************************************
+    Author: program to check given number is prime or not
+    ******************************************************************/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 81;
+            
+            for (int i = 2; i < n; i++)//check for prime
+            {
+                if (n % i == 0)
+                    break;
+
+
+                if (i == n)
+                {
+                    Console.WriteLine("prime number");
+                }
+                else
+                    Console.WriteLine("not prime number");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
