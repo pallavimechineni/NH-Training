3d7b4949057bc0ea3aa3b8956951728caf78ba1d
pallavimechineni <pallavi.velma@gmail.com>
2022-02-12 15:21:22 +0530

    program to print number after 1000 divisible by 97

---
+++ Day 14 programs/pgmonPrime_num/pgmusingBreak/pgmusingBreak/pgmOnbreak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+/********************************************************************
+ Author: M.Pallavi
+purpose : Find the first number after 1000 which is divisible by 97.
+*********************************************************************/
+
+namespace pgmOnbreak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1000; i <= 1097; i++)
+            {
+
+                if (i % 97 == 0) //check
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
