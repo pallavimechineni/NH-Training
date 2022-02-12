ff90ba61d0f1d53ca8431c8889003b4c0a041f49
pallavimechineni <pallavi.velma@gmail.com>
2022-02-12 18:36:28 +0530

    day 14 Assignment

--- Day 14 programs/pgmonPrime_num/pgmOnContinue/pgmOnContinue/Program.cs
+++ Day 14 programs/pgmonPrime_num/pgmOnContinue/pgmOnContinue/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace pgmOnContinue
 {
-    internal class Program
+/********************************************************************************************
+ Author: M.Pallavi    
+purpose: WACP to print numbers from 1 to 30 and skip the numbers divisible by 3 using  continue;
+**********************************************************************************************/
+internal class Program
     {
         static void Main(string[] args)
         {
+            int n;
+            Console.WriteLine("enter n value");
+            n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("numbers not divisible by 30 are");
+            for (int i = 0; i <= n; i++)
+            {
+                
+                if (i % 3 == 0)
+                    continue;
+                Console.Write(i + "\t");
+
+            }
+        Console.ReadLine();
         }
+
     }
 }
---
+++ Day 14 programs/pgmonSealedClass/sealedclasspgm/sealedclasspgm/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+/******************************************************
+ * Author: M.Pallavi
+ purpose: program on sealed class.
+********************************************************/
+
+namespace pgmonSealedClass
+{
+    sealed class police
+    {
+        public static int helpline = 100;
+        public int secret()
+        {
+            return 89;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            police p = new police();
+            Console.WriteLine(p.secret());
+        Console.ReadLine();
+        }
+
+       
+    }
+}
