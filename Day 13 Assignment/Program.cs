2df13f7b91da43ab2b934f7ff823da8723571a76
pallavimechineni <pallavi.velma@gmail.com>
2022-02-13 18:27:53 +0530

    program to print trace of array

---
+++ Day 13 Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pgmOnStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> data = new Stack<int>();
+            data.Push(1);
+            data.Push(2);
+            data.Push(3);
+            data.Push(4);
+            data.Push(5);
+            Console.WriteLine("total no.elements are " +data.Count);
+            Console.WriteLine("poped element is " +data.Pop());
+           Console.WriteLine("peek element is"+data.Peek());
+            Console.WriteLine("count is " +data.Count);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Day 13 Assignment/program3/program3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program3
+{
+    internal class Program
+    {   /* program on 2D array to print trace of array*/
+        static void Main(string[] args)
+        {
+
+            int[,] data = new int[,] { { 1, 2, 3 }, { 4, 1, 8 }, { 3, 4, 9 } };
+            int sum = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(data[i, j] + "  ");
+                }
+                Console.ReadLine();
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i == j)
+                    {
+                        sum = sum + data[i, j];
+                    }
+                }
+
+            }
+            Console.WriteLine("\n The Trace of the array is : {0}", sum);
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
+
