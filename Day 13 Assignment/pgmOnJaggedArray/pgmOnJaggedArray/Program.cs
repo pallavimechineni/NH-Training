614c2bd0482fcb5d2bd0831d2c0871b0d262021c
pallavimechineni <pallavi.velma@gmail.com>
2022-02-13 23:03:37 +0530

    program on queues

---
+++ Day 13 Assignment/pgmOnJaggedArray/pgmOnJaggedArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pgmOnJaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[][] names = new char[6][];
+
+            names[0] = new char[] { 'r', 'a', 'm','e','s','h' };
+            names[1] = new char[] { 'k', 'r', 'i', 't', 'i' };
+            names[2] = new char[] { 'n', 'b', 't', 'e', 'c', 'h', 'n', 'o', 'l', 'o', 'g', 'e', 's' };
+            names[3] = new char[] { 'p', 'a', 'l', 'l', 'a', 'v', 'i' };
+            names[4] = new char[] { 'c', 'h', 'a', 'i', 't', 'h', 'u' };
+            names[5] = new char[] { 'c', 'o', 'm', 'p', 'a', 'n', 'y' };
+            Console.WriteLine("printing array values ");
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < names[i].Length; j++)
+                {
+                    Console.Write(names[i][j]);
+                }
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
---
+++ Day 13 Assignment/pgmQueue/pgmQueue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pgmQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> data = new Queue<int>();
+            data.Enqueue(1);
+            data.Enqueue(2);
+            data.Enqueue(3);
+            data.Enqueue(4);
+            data.Enqueue(5);
+            data.Enqueue(6);
+            Console.WriteLine("no of elements in queue is " + data.Count);
+            Console.WriteLine("the element removed is " + data.Dequeue());
+            Console.WriteLine("last element is " + data.Peek());
+            Console.WriteLine("no of elements is " + data.Count);
+            Console.ReadLine();
+        }
+       
+
+    }
+}
+
+        
+    
+
