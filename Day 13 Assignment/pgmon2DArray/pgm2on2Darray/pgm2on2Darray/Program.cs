eb59f81ab8f3437d3168ba0496a7b449b020c155
pallavimechineni <pallavi.velma@gmail.com>
2022-02-13 18:06:33 +0530

    program on 2D array by intializing at declaration of array

---
+++ Day 13 Assignment/pgmon2DArray/pgm2on2Darray/pgm2on2Darray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace pgm2on2Darray
+{  /*Declare a 2-D array of size (3,2) and initialize in the same line while declaring and print the values using nested for loop*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] data = new int[,] { { 1, 2 }, { 4, 1 }, { 3, 4 } };
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(data[i, j] + "  ");
+                }
+                Console.Write("\n");
+            }
+             Console.ReadLine();
+        }
+    }
+
+}
