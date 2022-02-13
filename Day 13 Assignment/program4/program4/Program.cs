38c46ec35701c16d390ef55fb3105b01c516235e
pallavimechineni <pallavi.velma@gmail.com>
2022-02-13 18:48:02 +0530

    program on printing array values

---
+++ Day 13 Assignment/program4/program4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] data = new int[2, 2];
+
+            
+            for (int i = 0; i < 2; i++)// Reading elements from the user
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write("Enter the array elements ");
+                    data[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            // Printing the Array elements
+            Console.WriteLine(" array items are");
+            for (int i = 0; i < 2; i++)
+
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write("\t" + data[i, j] + " ");
+                }
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
