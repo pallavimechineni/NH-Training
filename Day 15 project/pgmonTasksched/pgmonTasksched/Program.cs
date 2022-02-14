4414fc506d7b96d3128b804a7048fdef2b75db75
pallavimechineni <pallavi.velma@gmail.com>
2022-02-14 11:57:17 +0530

    program on task schedular

---
+++ Day 15 project/pgmonTasksched/pgmonTasksched/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pgmonTasksched
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        { 
+            string filePath = @"D:\Day1 Assessment by Pallavi Mechineni\Day 15 project\filepgm.txt";
+
+            string TaskSchedPath = @"C:\Users\Administrator\OneDrive\Desktop\ConsoleApp4\filepgm2.txt";
+            if (File.Exists(TaskSchedPath))
+                File.Delete(TaskSchedPath);
+
+            File.Copy(filePath, TaskSchedPath);
+
+            Console.WriteLine("File Copying is Done");
+
+            
+            Console.ReadKey();
+
+
+
+
+        }
+  
+    }
+
+}
