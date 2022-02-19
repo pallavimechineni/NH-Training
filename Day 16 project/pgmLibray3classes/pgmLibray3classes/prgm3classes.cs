2b000bbeda76c38a4c60d1934e19bdd53d387c20
pallavimechineni <pallavi.velma@gmail.com>
2022-02-19 15:14:42 +0530

    program  to create class library with 3 classes

---
+++ Day 16 project/pgmLibray3classes/pgmLibray3classes/prgm3classes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pgmLibray3classes
+{
+    public class mathematics
+    {
+        int input;
+        public void readdata()
+        {
+            Console.WriteLine("enter value");
+            input = Convert.ToInt32(Console.ReadLine());
+        }
+        public int factorial()
+        {
+            int fact = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+    public class chemistry
+    {
+        public string benzene()
+        {
+            return "C6H6";
+        }
+        public string water()
+        {
+            return "H20";
+        }
+    }
+    public class physics
+    {
+        public int Velocity(int u, int a, int t)
+        {
+            return u + a * t;
+        }
+    }
+
+
+}    
