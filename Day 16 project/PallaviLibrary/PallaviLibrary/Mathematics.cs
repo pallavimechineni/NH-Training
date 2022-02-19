adee3954a18830863e431d8fab3d61f5e33f4e0b
pallavimechineni <pallavi.velma@gmail.com>
2022-02-19 15:09:04 +0530

    program to create a class Library

---
+++ Day 16 project/PallaviLibrary/PallaviLibrary/Mathematics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PallaviLibrary
+{
+    public class Mathematics
+    {
+        int input;
+        public void readdata()
+        {
+            Console.WriteLine("enter value");
+            input =Convert.ToInt32(Console.ReadLine());
+        }
+        public int factorial()
+        {
+            int fact = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+
+    }
+}
