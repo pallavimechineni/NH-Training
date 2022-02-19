852e50d72d0a6ee08cf989406d3825df7c713974
pallavimechineni <pallavi.velma@gmail.com>
2022-02-19 15:17:43 +0530

    program to create console application to refer those 3 classes

---
+++ Day 16 project/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using pgmLibray3classes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            mathematics m = new mathematics();
+            m.readdata();
+            Console.WriteLine("factorial of number is" +m.factorial());
+            physics p = new physics();
+            Console.WriteLine("velocity is " +p.Velocity(3, 4, 5));
+            chemistry c = new chemistry();
+            Console.WriteLine("benezene value is " +c.benzene());
+            Console.WriteLine("water formula is" +c.water());
+            Console.ReadLine();
+        }
+
+
+    }
+ }
+    
+
