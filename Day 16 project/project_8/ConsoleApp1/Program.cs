ff5289a60dfa3c27588ae435393076dc2e2a45e4
pallavimechineni <pallavi.velma@gmail.com>
2022-02-19 16:51:26 +0530

    program to use 2 LIBRARY class in one console application

---
+++ Day 16 project/project_8/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using mathlibrary;
+using phylibrary;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathematics m = new Mathematics();
+            m.Readdata();
+            Console.WriteLine("factorial of number is{0}", + m.Factorial());
+            Physics p = new Physics();
+            Console.WriteLine("velocity is " + p.Velocity(3, 4, 5));
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Day 16 project/project_8/mathlibrary/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathlibrary
+{
+    public class Mathematics
+    {
+        int input;
+        public void Readdata()
+        {
+            Console.WriteLine("enter value");
+            input = Convert.ToInt32(Console.ReadLine());
+        }
+        public int Factorial()
+        {
+            int fact = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+
+}
