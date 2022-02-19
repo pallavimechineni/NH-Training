6e621937b037e246070077924eb3945ba305e57e
pallavimechineni <pallavi.velma@gmail.com>
2022-02-19 15:28:47 +0530

    program to print hello world by using object

---
+++ Day 16 project/program1/program1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {   
+        public void Print()
+        {
+            Console.WriteLine("hello world");//printing hello world
+        }
+        static void Main(string[] args)
+        { 
+            Program p=new Program();//creating object for class program
+            p.Print();//calling method  by using object
+            Console.ReadLine();
+        }
+      
+    }
+}
