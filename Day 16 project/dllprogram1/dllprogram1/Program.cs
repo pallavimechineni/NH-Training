ad1b632e14041390742d3479e64edd389ac4cc1f
pallavimechineni <pallavi.velma@gmail.com>
2022-02-19 15:11:25 +0530

    program on class mathematics to find factorial of number

---
+++ Day 16 project/dllprogram1/dllprogram1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using PallaviLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dllprogram1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathematics m = new Mathematics();
+            m.readdata();
+            Console.WriteLine(m.factorial());
+            Console.ReadLine();
+        }
+
+
+    }
+}        
+    
+
