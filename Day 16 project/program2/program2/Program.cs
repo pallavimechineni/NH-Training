49bc47029edc584f8d32d7239921d93bc033a811
pallavimechineni <pallavi.velma@gmail.com>
2022-02-19 15:53:15 +0530

    program on factorial of number by using object

---
+++ Day 16 project/program2/program2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    class Factorial
+    {
+        int n;
+        public void Readinput()
+        {
+            Console.WriteLine("enter number");
+            n = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public int Fact()
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+
+            }
+            return fact;
+           
+        }
+        static void Main(string[] args)
+        {
+            Factorial f = new Factorial();
+            f.Readinput();
+            Console.WriteLine("{0}",f.Fact());
+            Console.ReadLine();
+        }
+       
+    }
+}
+
