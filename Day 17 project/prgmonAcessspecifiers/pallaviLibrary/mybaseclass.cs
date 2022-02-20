d5e3b622d64c73bcb670cb30c0ac9a6a6127e4ce
pallavimechineni <pallavi.velma@gmail.com>
2022-02-20 19:48:44 +0530

    pgm on Accessspecifiers

---
+++ Day 17 project/prgmonAcessspecifiers/pallaviLibrary/mybaseclass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pallaviLibrary
+{
+    public  class mybaseclass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        int e;
+        public void method()
+        {
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+
+
+        }
+    }
+    public class myderivedclass : mybaseclass
+    {
+        public void myderivedmethod()
+        {   /*
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+            */
+
+        }
+    }
+    public class Otherclass
+    {
+        public void otherclass()
+        {
+            /*
+
+        mybaseclass mb = new mybaseclass();
+        mb.a  = 1;
+        mb.b =2;
+        mb.c=3;
+        mb.d=4;
+         mb.e=5;
+
+           */
+        }
+
+    }
+}
+    
+
---
+++ Day 17 project/prgmonAcessspecifiers/publiclibrary/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using pallaviLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace publiclibrary
+{
+    public class Class1 : mybaseclass
+    {
+        public void derivedmethod()
+        {
+
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+
+        }
+    }
+    public class otherclass
+    {
+        public void othermethod()
+        {
+             mybaseclass m=new mybaseclass();   
+            m.a = 1;
+            m.b = 2;
+            m.c = 3;
+           m. d = 4;
+            m.e = 5;
+
+        }
+    }
+}
+
