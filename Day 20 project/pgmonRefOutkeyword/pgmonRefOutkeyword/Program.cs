5de486a1c51bdcce11f22f0cc6f6a2d45be32f2b
pallavimechineni <pallavi.velma@gmail.com>
2022-02-20 17:43:18 +0530

    program on Ref and Out keywords

---
+++ Day 20 project/pgmonRefOutkeyword/pgmonRefOutkeyword/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+// Author : M.Pallavi
+// Purpose : C# Code to illustrate, the Ref & Out keywords.
+namespace ConsoleApplication1
+{
+    class Refexam
+    {
+        public void show(ref int x, out int y)
+        {
+            y = x % 2;
+            x = x / 2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int reminder;
+            Refexam obj = new Refexam();
+            
+            a = 35;
+            Console.WriteLine("before call a :" + a);// this will display 35
+                                                     
+            obj.show(ref a, out reminder);
+            Console.WriteLine("after call a :" + a);// this will display 17
+            Console.WriteLine("before call a :" + reminder); 
+            Console.ReadKey();
+        }
+    }
+}
