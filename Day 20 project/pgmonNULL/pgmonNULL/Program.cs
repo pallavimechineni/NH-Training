6524e02893d8bed4459400eae3991142e34ff259
pallavimechineni <pallavi.velma@gmail.com>
2022-02-20 17:28:08 +0530

    program on NULL TYPE

---
+++ Day 20 project/pgmonNULL/pgmonNULL/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+// Author : M.Pallavi
+// Purpose : Nullable Type C# Code Example.
+
+namespace NullableTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int? firstValue = 20;
+            int? secondValue = null;
+
+            if (firstValue.HasValue)
+            {
+                Console.WriteLine(firstValue.Value);
+
+            }
+            if (secondValue.HasValue)
+            {
+                Console.WriteLine(secondValue.Value);
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
