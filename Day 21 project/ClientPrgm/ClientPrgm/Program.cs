e0a11b48c65684f60fc5c3014366fcc356b9597a
pallavimechineni <pallavi.velma@gmail.com>
2022-02-21 12:23:59 +0530

    program2 on client  application

---
+++ Day 21 project/ClientPrgm/ClientPrgm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ClientPrgm.ServiceReference1;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPrgm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WebService1SoapClient w = new WebService1SoapClient();
+           Console.WriteLine(w.Add(1, 2));
+            Console.WriteLine(w.Div(1, 2));
+            Console.WriteLine(w.Mul(1, 2)) ;
+            Console.WriteLine(w.Factorial(5));
+            Console.ReadLine();
+        }
+    }
+}
