661b45b04a7cbf540cab1294e546ef27ec4077ac
pallavimechineni <pallavi.velma@gmail.com>
2022-02-21 09:58:37 +0530

    program to create C# code to read xml file and print only employee names from the xml

---
+++ Day 19 project/pgmonXml/pgmonXml/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+namespace pgmonXml
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("C:\\Users\\Administrator\\OneDrive\\Desktop\\emp2.xml");
+            // XmlNode node = doc.DocumentElement.SelectSingleNode(\employees\employee);
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+               
+                    string text = node.InnerXml;
+              
+           
+           }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
---
+++ Day 19 project/program2/program2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml;
+
+// Author : M.Pallavi
+// Purpose : C# Code To read Xml Data from a Xml file & printing the data of Only Names to the Console.
+
+namespace Day19Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"C:\Users\Administrator\OneDrive\Desktop\emp1.xml");
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                foreach (XmlNode childNode in node.ChildNodes)
+                {
+                    if (childNode.Name == "Name")
+                    {
+                        Console.WriteLine(childNode.InnerText);
+                    }
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
