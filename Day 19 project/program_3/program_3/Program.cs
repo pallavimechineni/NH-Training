2d206260abe0e7ca4d376e5484fd680d5cc2e90f
pallavimechineni <pallavi.velma@gmail.com>
2022-02-21 10:08:20 +0530

    program to print data from XML

---
+++ Day 19 project/program_3/program_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml;
+
+// Author : M.Pallavi
+// Purpose : C# Code To read Xml Data from a Xml file & printing the data of Only Names with data
+
+namespace Day19Project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"C:\Users\Administrator\OneDrive\Desktop\emp1.xml");
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                foreach (XmlNode childNode in node.ChildNodes)
+                {
+                    if (childNode.Name == "ID")
+                    {
+                        Console.Write("{0}, ", childNode.InnerText);
+                    }
+                    if (childNode.Name == "Name")
+                    {
+                        Console.Write("{0}, ", childNode.InnerText);
+                    }
+                    if (childNode.Name == "Price")
+                    {
+                        Console.Write("{0}\n", childNode.InnerText);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
